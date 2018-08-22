dd98ec2f057faa8b849b7d98547addec6b7d2ce1
Joseph Lisee <jlisee@gmail.com>
2018-08-22 08:56:04 -0400

    Schema validation tool and start of schema files (#18)
    Run `node ./schema/validate.js` to compare the files in
    `schema/examples` to matching schema files in `schema`.

---
+++ schema/validate.js
@@ -0,0 +1,125 @@
+// Copyright (c) 2019 Uber Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* eslint no-console: off */
+/* eslint-env node, browser */
+
+const path = require('path');
+const walk = require('walk');
+const Ajv = require('ajv');
+const fs = require('fs');
+
+function main() {
+  const ajv = new Ajv();
+
+  const schema_dir = path.resolve(__dirname);
+
+  load_all_schemas(ajv, schema_dir);
+
+  const examples_dir = path.join(schema_dir, 'examples');
+
+  validate_examples(ajv, examples_dir);
+}
+
+function load_all_schemas(ajv, schema_dir) {
+  const schema_options = {
+    listeners: {
+      file(fpath, stat, next) {
+        if (stat.name.endsWith('.schema.json')) {
+          // Build the path to the matching schema
+          const full_path = path.join(fpath, stat.name);
+          const rel_path = path.relative(schema_dir, full_path);
+
+          try {
+            load_schema(ajv, schema_dir, rel_path);
+          } catch (e) {
+            console.log(`Error loading schema: ${rel_path} ${e}`);
+          }
+        }
+        next();
+      }
+    }
+  };
+
+  walk.walkSync(schema_dir, schema_options);
+}
+
+function load_schema(ajv, schema_dir, relative_path) {
+  // Load the Schema
+  const schema_path = path.join(schema_dir, relative_path);
+
+  console.log(`Load: ${relative_path}`);
+
+  let schema;
+  try {
+    const schema_contents = fs.readFileSync(schema_path);
+
+    schema = JSON.parse(schema_contents);
+  } catch (e) {
+    throw new Error(`Error parsing: ${schema_path} ${e}`);
+  }
+
+  ajv.addSchema(schema, relative_path);
+}
+
+function validate_examples(ajv, examples_dir) {
+  const options = {
+    listeners: {
+      file(fpath, stat, next) {
+        if (!stat.name.endsWith('~')) {
+          // Build the path to the matching schema
+          const example_path = path.join(fpath, stat.name);
+          try {
+            validate_example(ajv, examples_dir, example_path);
+          } catch (e) {
+            console.log(`Error validating: ${example_path} ${e}`);
+          }
+        }
+        next();
+      }
+    }
+  };
+
+  walk.walkSync(examples_dir, options);
+}
+
+function validate_example(ajv, examples_dir, example_path) {
+  const example_rel_path = path.relative(examples_dir, example_path);
+
+  const schema_rel_path = example_rel_path.replace('.json', '.schema.json');
+
+  // Load the JSON to validate
+  const contents = fs.readFileSync(example_path);
+  let data;
+  try {
+    data = JSON.parse(contents);
+  } catch (e) {
+    throw new Error(`Error parsing: example_path} ${e}`);
+  }
+
+  // Lookup the schema and validate
+  const validate = ajv.getSchema(schema_rel_path);
+  const valid = validate(data);
+
+  if (!valid) {
+    console.log(`Fail: ${example_path}`);
+    console.log(`Schema: ${schema_rel_path}`);
+    console.log(validate.errors);
+  } else {
+    console.log(`Pass: ${example_path}`);
+  }
+}
+
+// Invoke
+main();
