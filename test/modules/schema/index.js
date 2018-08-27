0a588d7157572f9105340cfd3e364bcfc3a0fc7f
Joseph Lisee <jlisee@uberatc.com>
2018-08-26 22:08:54 -0400

    Move schema into xviz/schema module add tests
    Switch from having a CLI tool to using a tape test to validate our
    examples.  We also have a general test for our validation module
    itself, independent of validating our examples.

---
+++ test/modules/schema/index.js
@@ -0,0 +1,2 @@
+// Set up a configuration (TODO/OSS - this should be a neutral config)
+import './validator.spec';
---
+++ test/modules/schema/validator.spec.js
@@ -0,0 +1,29 @@
+import {validateExampleFiles} from '@xviz/schema';
+import test from 'tape-catch';
+import * as path from 'path';
+
+test('validateExamplesFiles', t => {
+  const dataDir = path.join(__dirname, 'data');
+  const schemaDir = path.join(dataDir, 'schema');
+  const examplesDir = path.join(dataDir, 'examples');
+
+  const badExamples = path.join(examplesDir, 'bad');
+  t.notOk(validateExampleFiles(schemaDir, badExamples), 'bad example fails');
+
+  const goodExamples = path.join(examplesDir, 'good');
+  t.ok(validateExampleFiles(schemaDir, goodExamples), 'good example passes');
+
+  t.end();
+});
+
+test('validateXVIZExamples', t => {
+  // Do it by directory path first
+  const schemaDir = path.join(__dirname, '..', '..', '..', 'modules', 'schema');
+  const examplesDir = path.join(schemaDir, 'examples');
+
+  t.ok(validateExampleFiles(schemaDir, examplesDir), 'all examples match schema');
+
+  // TODO: setup imports/exports for schema files
+
+  t.end();
+});
