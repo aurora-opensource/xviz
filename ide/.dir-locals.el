c5eee0ddad29b9f8c01500adadbe21d499234f1b
Joseph Lisee <jlisee@gmail.com>
2018-09-03 22:02:09 -0400

    Emacs specific settings (#47)
    I am not putting this in the root because other emacs users might
    consider it invasive.  Instead we check it into a subfolder that an
    emacs user can instead symlink too.
    
    This is similar to what we do with vscode, but instead we actually
    check the files in for others to share.

---
+++ ide/.dir-locals.el
@@ -0,0 +1,10 @@
+;;; Emacs settings to adjust defaults to match this repository
+;;; use by manually symlinking this file from the root of the
+;;; repository.
+((nil . ((tab-width . 2)
+         (js-indent-level . 2)))
+
+ (js2-mode . ((tab-width . 2)
+             (js-indent-level . 2)))
+
+ (html-mode . ((tab-width . 2))))
