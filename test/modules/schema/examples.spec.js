b7809fae5aa2fcf993b74d7e5738f9c319790ca3
Joseph Lisee <jlisee@gmail.com>
2018-09-05 13:23:29 -0400

    Modularize and flesh out more of the schema (#49)
    This is not a complete update, but it's enough that we have the full
    stack running and we can do review. It includes these steps:
    
     - session_metadata
     - camera_info
     - state_update
     - stream_set
     - style_class
     - style_value
     - circle primitive
     - math types: vector3x1, matrix3x3, matrix4x4
    
    The validation JS code now supports:
    
     - Directories of examples
     - Invalid examples (ie. things that should fail validation)
     - More explicit error reporting
    
    Some typos in examples from the docs were fixed as well.

--- test/modules/schema/examples.spec.js
+++ test/modules/schema/examples.spec.js
@@ -1,15 +1,16 @@
-import {validateExampleFiles} from '@xviz/schema';
+import {validateExampleFiles, validateInvalidFiles} from '@xviz/schema';
 import test from 'tape-catch';
 import * as path from 'path';
 
-test('validateXVIZExamples', t => {
+test.only('validateXVIZExamples', t => {
   // Do it by directory path first
   const schemaDir = path.join(__dirname, '..', '..', '..', 'modules', 'schema');
   const examplesDir = path.join(schemaDir, 'examples');
 
   t.ok(validateExampleFiles(schemaDir, examplesDir), 'all examples match schema');
 
-  // TODO: setup imports/exports for schema files
+  const invalidDir = path.join(schemaDir, 'invalid');
+  t.ok(validateInvalidFiles(schemaDir, invalidDir), 'all invalid examples fail');
 
   t.end();
 });
