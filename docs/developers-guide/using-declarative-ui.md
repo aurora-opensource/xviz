f5535a5bf559cff255a9d8c47c0f51377bdd69b2
Timothy Wojtaszek <twojtasz@uber.com>
2018-10-09 02:59:34 -0700

    Update documentation (#123)
    - Move developer guide higher in the side bar
    - Fix json tag issue, changed to js

--- docs/developers-guide/using-declarative-ui.md
+++ docs/developers-guide/using-declarative-ui.md
@@ -1,6 +1,6 @@
 # Using the Declarative UI
 
-XVIZ allows the producer to declartively specify UI components like tables and tree-views. These UI components can optionally be rendered by the XVIZ client, and can be used to provide additional information beyond what can be captured in the primary 3D scene.
+XVIZ allows the producer to declartively connect strams to UI components like control widgets, tables, charts, and tree-views. These UI components can be rendered by the XVIZ client, and can be used to provide additional information beyond what can be captured in the primary 3D scene.
 
 The easiest way to add declarative UI components to your XVIZ in JavaScript is to use the `XVizUIBuilder` class.
 
--- website/src/config.js
+++ website/src/config.js
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export const PROJECT_TYPE = 'phab';
+export const PROJECT_TYPE = 'github';
+export const PROJECT_ORG = 'uber';
+export const PROJECT_NAME = 'xviz';
 
-export const PROJECT_NAME = 'XVIZ';
-
-export const PROJECT_URL = 'https://code.uberinternal.com/diffusion/VIATCS/';
+export const PROJECT_URL = 'https://github.com/uber/xviz';
 export const PROJECT_DESC = 'XVIZ Protocol';
 
 export const PROJECTS = {};
@@ -25,6 +25,12 @@ export const HOME_HEADING = 'XVIZ Protocol';
 
 export const HOME_RIGHT = null;
 
-export const HOME_BULLETS = [];
+export const HOME_BULLETS = [{
+  text: 'Visualize autonomy and robotics data',
+  desc: 'Describe geometry, objects, and metrics that support interactive exploration of your data.'
+}, {
+  text: 'Interface controls defined along with the data',
+  desc: 'Easing development for robotics engineers and across robotic system releases.'
+}];
 
 export const ADDITIONAL_LINKS = [];
