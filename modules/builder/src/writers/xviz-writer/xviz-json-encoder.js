ce9457adb0b7797c87f1c8237130206db573c9d9
Timothy Wojtaszek <twojtasz@uber.com>
2018-10-14 21:26:34 -0700

    Add 'options' parameter to XVIZWriter to control the format of the output (#134)
    Binary is default. JSON has the following changes:
    - typed arrays are converted to arrays
    - typed arrays for primitive's with a key 'vertices' are turned into
      nested arrays
    - images are converted to base64 strings
    - add option 'precision' to have Number values in the JSON fixed
      (dflt: 9 decimal places)

---
+++ modules/builder/src/writers/xviz-writer/xviz-json-encoder.js
@@ -0,0 +1,54 @@
+import base64js from 'base64-js';
+
+// Recursively walk object performing the following conversions
+// - primitives with typed array fields are turned into arrays
+// - primtives of type image have the data turned into a base64 string
+export function xvizConvertJson(object, keyName) {
+  if (Array.isArray(object)) {
+    return object.map(element => xvizConvertJson(element));
+  }
+
+  // Typed arrays become normal arrays
+  // TODO: no way to know if this should be 3 or 4
+  if (ArrayBuffer.isView(object)) {
+    // Return normal arrays
+    if (keyName !== 'vertices') {
+      return Array.from(object);
+    }
+
+    // For primitives with key's 'vertices', we force nested arrays.
+    // TODO(twojtasz): Support flat arrays
+    const length = object.length;
+    if (length % 3 !== 0) {
+      throw new Error('TypeArray conversion failure. The array is expect to be divisible by 3');
+    }
+
+    // Construct points from flattened array
+    const newObject = [];
+    const count = length / 3;
+    for (let i = 0; i < count; i++) {
+      newObject.push(Array.from(object.slice(i * 3, i * 3 + 3)));
+    }
+    return newObject;
+  }
+
+  if (object !== null && typeof object === 'object') {
+    // Handle XVIZ Image Primitive
+    const properties = Object.keys(object);
+    if (['type', 'data'].every(prop => properties.includes(prop)) && object.type === 'image') {
+      return {
+        ...object,
+        data: base64js.fromByteArray(object.data)
+      };
+    }
+
+    // Handle all other objects
+    const newObject = {};
+    for (const key in object) {
+      newObject[key] = xvizConvertJson(object[key], key);
+    }
+    return newObject;
+  }
+
+  return object;
+}
--- modules/builder/src/writers/xviz-writer/xviz-writer.js
+++ modules/builder/src/writers/xviz-writer/xviz-writer.js
@@ -1,22 +1,52 @@
 import {writeBinaryXVIZtoFile} from './xviz-binary-writer';
+import {xvizConvertJson} from './xviz-json-encoder.js';
 
 export default class XVIZWriter {
   // xvizMetadata is the object returned
   // from a Builder.
-  writeMetadata(xvizDirectory, xvizMetadata) {
+  writeMetadata(xvizDirectory, xvizMetadata, options = {writeBinary: true, writeJson: false}) {
     const fs = module.require('fs');
     const path = module.require('path');
     const xvizMetadataFilename = path.join(xvizDirectory, '1-frame');
-    writeBinaryXVIZtoFile(xvizMetadataFilename, xvizMetadata, {flattenArrays: false});
-    fs.writeFileSync(`${xvizMetadataFilename}.json`, JSON.stringify(xvizMetadata, null, 2), {
-      flag: 'w'
-    });
+    if (options.writeBinary) {
+      writeBinaryXVIZtoFile(xvizMetadataFilename, xvizMetadata, {flattenArrays: false});
+    }
+
+    if (options.writeJson) {
+      fs.writeFileSync(`${xvizMetadataFilename}.json`, JSON.stringify(xvizMetadata, null, 2), {
+        flag: 'w'
+      });
+    }
   }
 
-  writeFrame(xvizDirectory, frameNumber, xvizFrame) {
+  writeFrame(
+    xvizDirectory,
+    frameNumber,
+    xvizFrame,
+    options = {writeBinary: true, writeJson: false, precision: 9}
+  ) {
+    const fs = module.require('fs');
     const path = module.require('path');
     // +2 is because 1 is metadata, so we start with 2
     const frameFilePath = path.join(xvizDirectory, `${frameNumber + 2}-frame`);
-    writeBinaryXVIZtoFile(frameFilePath, xvizFrame, {flattenArrays: false});
+    if (options.writeBinary) {
+      writeBinaryXVIZtoFile(frameFilePath, xvizFrame, {flattenArrays: false});
+    }
+
+    if (options.writeJson) {
+      // Limit precision to save space
+      const numberRounder = (k, value) => {
+        if (typeof value === 'number') {
+          return Number(value.toFixed(options.precision));
+        }
+
+        return value;
+      };
+
+      const jsonXvizFrame = xvizConvertJson(xvizFrame);
+      fs.writeFileSync(`${frameFilePath}.json`, JSON.stringify(jsonXvizFrame, numberRounder), {
+        flag: 'w'
+      });
+    }
   }
 }
