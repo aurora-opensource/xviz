078b0d14fbe9b2dc5f356be4b693ccfa6c559a90
Joseph Lisee <jlisee@gmail.com>
2018-11-09 19:45:10 -0500

    Define and update TreeTable schema (#191)
    We have translated the documented schema into a formal JSON and
    protobuf schema with a set of invalid and valid examples.
    
    The schema has changed to having all column values encoded as strings
    for now.  In the future when we can figure out a more efficient
    protobuf compatible encoding we'll make a backwards compatible switch
    to that.

--- modules/schema/src/data.js
+++ modules/schema/src/data.js
@@ -24,6 +24,7 @@ import corePrimitiveBaseSchemaJson from '../core/primitive_base.schema.json';
 import corePrimitiveStateSchemaJson from '../core/primitive_state.schema.json';
 import coreStreamSetSchemaJson from '../core/stream_set.schema.json';
 import coreTimeseriesStateSchemaJson from '../core/timeseries_state.schema.json';
+import coreUiPrimitiveStateSchemaJson from '../core/ui_primitive_state.schema.json';
 import coreValueSchemaJson from '../core/value.schema.json';
 import coreValuesSchemaJson from '../core/values.schema.json';
 import coreVariableSchemaJson from '../core/variable.schema.json';
@@ -57,6 +58,7 @@ import styleClassSchemaJson from '../style/class.schema.json';
 import styleColorSchemaJson from '../style/_color.schema.json';
 import styleObjectValueSchemaJson from '../style/object_value.schema.json';
 import styleStreamValueSchemaJson from '../style/stream_value.schema.json';
+import uiPrimitivesTreetableSchemaJson from '../ui-primitives/treetable.schema.json';
 
 export const SCHEMA_DATA = {
   'core/annotation_base.schema.json': coreAnnotationBaseSchemaJson,
@@ -69,6 +71,7 @@ export const SCHEMA_DATA = {
   'core/primitive_state.schema.json': corePrimitiveStateSchemaJson,
   'core/stream_set.schema.json': coreStreamSetSchemaJson,
   'core/timeseries_state.schema.json': coreTimeseriesStateSchemaJson,
+  'core/ui_primitive_state.schema.json': coreUiPrimitiveStateSchemaJson,
   'core/value.schema.json': coreValueSchemaJson,
   'core/values.schema.json': coreValuesSchemaJson,
   'core/variable.schema.json': coreVariableSchemaJson,
@@ -101,5 +104,6 @@ export const SCHEMA_DATA = {
   'style/class.schema.json': styleClassSchemaJson,
   'style/_color.schema.json': styleColorSchemaJson,
   'style/object_value.schema.json': styleObjectValueSchemaJson,
-  'style/stream_value.schema.json': styleStreamValueSchemaJson
+  'style/stream_value.schema.json': styleStreamValueSchemaJson,
+  'ui-primitives/treetable.schema.json': uiPrimitivesTreetableSchemaJson
 };
