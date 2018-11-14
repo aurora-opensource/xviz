5697cfad078e90d09032c3abf67c7eeb2d2a059e
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2018-11-14 11:17:32 -0800

    Discard image data after parsing (#202)

--- modules/parser/src/workers/stream-data.worker.js
+++ modules/parser/src/workers/stream-data.worker.js
@@ -13,35 +13,6 @@
 // limitations under the License.
 
 /* global self */
-import {setXVIZConfig, setXVIZSettings} from '../config/xviz-config';
-import {parseStreamDataMessage} from '../parsers/parse-stream-data-message';
-import {preSerialize} from '../parsers/serialize';
-function onResult(message) {
-  const transfers = [];
-  const {channels} = message;
-  if (channels) {
-    for (const streamName in channels) {
-      const {pointCloud} = channels[streamName];
-      if (pointCloud) {
-        transfers.push(
-          pointCloud.ids.buffer,
-          pointCloud.colors.buffer,
-          pointCloud.positions.buffer
-        );
-      }
-    }
-  }
-  message = preSerialize(message);
-  self.postMessage(message, transfers);
-}
-function onError(error) {
-  throw error;
-}
-self.onmessage = e => {
-  if (e.data.xvizConfig) {
-    setXVIZConfig(e.data.xvizConfig);
-    setXVIZSettings(e.data.xvizSettings);
-  } else {
-    parseStreamDataMessage(e.data, onResult, onError);
-  }
-};
+import getStreamDataWorker from './stream-data-worker';
+
+getStreamDataWorker({})(self);
