7d35a0af0bdce11059475759d6617290ac938ceb
Xiaoji Chen <xiaoji@uber.com>
2018-12-27 15:10:52 -0800

    add comments

--- modules/parser/src/parsers/parse-vehicle-pose.js
+++ modules/parser/src/parsers/parse-vehicle-pose.js
@@ -28,12 +28,16 @@ export function parseVehiclePose(vehiclePose, opts = {}) {
 export function getTransformsFromPose(vehiclePose) {
   const {longitude, latitude, altitude = 0} = vehiclePose;
 
-  const origin = Number.isFinite(vehiclePose.longitude) && Number.isFinite(vehiclePose.latitude) ?
-    [longitude, latitude, altitude] : null;
+  const origin =
+    Number.isFinite(vehiclePose.longitude) && Number.isFinite(vehiclePose.latitude)
+      ? [longitude, latitude, altitude]
+      : null;
   const pose = new Pose(vehiclePose);
 
   const vehicleRelativeTransform = pose.getTransformationMatrix();
 
+  // If mapOrigin is not specified, use a faux position of [0, 0, 0]
+  // deck.gl needs a lon/lat position to target the camera
   const trackPosition = addMetersToLngLat(
     origin || [0, 0, 0],
     vehicleRelativeTransform.transformVector([0, 0, 0])
