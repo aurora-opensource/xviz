aa8981814e56f7be3196f18cb66e2f35116a771e
Tim Wojtaszek <twojtasz@uber.com>
2019-01-06 16:11:40 -0800

    Add script to generate website demo data
    - ensures the data is easily reproduced
    - places them directly in the expected location for easy submission

---
+++ examples/converters/kitti/website-demo-data.sh
@@ -0,0 +1,6 @@
+yarn start \
+-d ../../../data/kitti/2011_09_26/2011_09_26_drive_0005_sync \
+-o ../../../../xviz-data/kitti/2011_09_26_drive_0005_sync \
+--fake-streams \
+--disable-streams=image_00,image_01
+
---
+++ examples/converters/nutonomy/website-demo-data.sh
@@ -0,0 +1,5 @@
+yarn start \
+-d ../../../data/nutonomy/v0.1 \
+--samples-directory=../../../data/nutonomy/samples \
+-o ../../../../xviz-data/nutonomy \
+--scenes=6
