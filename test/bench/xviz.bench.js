46d6f0010e945b4dc8f3cfafb27fa3ead8b0efe4
Shanshan Zhu <sszhupku@gmail.com>
2019-02-20 15:40:51 -0800

    Allow sending nested JSON string to parseStreamMessage (#339)

--- test/bench/xviz.bench.js
+++ test/bench/xviz.bench.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* global Buffer */
+/* eslint-disable camelcase */
 import sampleXVIZMetadata from 'test-data/sample-metadata-message.json';
 import sampleXVIZSnapshot from 'test-data/sample-xviz.json';
 import sampleXVIZStylesheet from 'test-data/xviz-style-sheet.json';
@@ -39,10 +41,29 @@ function parse(message, opts) {
   return result;
 }
 
+const getBuffer = xvizString => {
+  const buf = new Buffer(xvizString);
+  const length = buf.byteLength / Uint8Array.BYTES_PER_ELEMENT;
+  return new Uint8Array(buf.buffer, buf.byteOffset, length);
+};
+const xvizTestString = JSON.stringify(sampleXVIZSnapshot.updates);
+const xvizTestBuffer = getBuffer(xvizTestString);
+const stringTestJSON = {
+  update_type: sampleXVIZSnapshot.update_type,
+  updates: xvizTestString
+};
+
+const binaryTestJSON = {
+  update_type: sampleXVIZSnapshot.update_type,
+  updates: xvizTestBuffer
+};
+
 export default function xvizBench(bench) {
   return bench
     .group('PARSE XVIZ')
     .add('xviz#parseMetadata', () => parse(sampleXVIZMetadata))
     .add('xviz#parseFrame', () => parse(sampleXVIZSnapshot))
+    .add('xviz#parseFrame_StringJSON', () => parse(stringTestJSON))
+    .add('xviz#parseFrame_BinaryJSON', () => parse(binaryTestJSON))
     .add('xviz#parseStylesheet', () => new XVIZStyleParser(sampleXVIZStylesheet));
 }
