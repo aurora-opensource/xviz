78025451bef8e932b986b49939f5b354a5b01692
Tim Wojtaszek <twojtasz@uber.com>
2019-04-04 14:09:48 -0700

    Remove files that have moved to @xviz/io

--- modules/builder/src/index.js
+++ modules/builder/src/index.js
@@ -15,10 +15,6 @@
 // UTILS
 export {loadUri} from './utils/load-uri.js';
 
-// WRITERS
-export {default as XVIZWriter} from './writers/xviz-writer/xviz-writer';
-export {encodeBinaryXVIZ} from './writers/xviz-writer/xviz-binary-writer';
-
 // BUILDERS
 export {default as XVIZBuilder} from './builders/xviz-builder';
 export {default as XVIZMetadataBuilder} from './builders/xviz-metadata-builder';
--- test/modules/builder/index.js
+++ test/modules/builder/index.js
@@ -12,7 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import './xviz-loader';
 import './builders';
 import './utils';
-import './writers';
