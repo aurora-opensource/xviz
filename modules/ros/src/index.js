1ea66275ddc0665f5a04abd8ed7ead33b850aff6
Tim Wojtaszek <twojtasz@uber.com>
2019-04-17 12:47:55 -0700

    Export XVIZ Provider for ROS module

--- modules/ros/src/index.js
+++ modules/ros/src/index.js
@@ -12,3 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 export {Bag} from './bag';
+export {ROSBAGDataProvider} from './providers/rosbag-data-provider';
