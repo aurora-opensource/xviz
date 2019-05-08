63876372057d21dddc394f7443d76104a0668cb7
Tim Wojtaszek <twojtasz@uber.com>
2019-05-08 09:09:06 -0700

    small refactor on flattenObject() func

--- modules/builder/src/writers/xviz-writer/xviz-pack-binary.js
+++ modules/builder/src/writers/xviz-writer/xviz-pack-binary.js
@@ -66,25 +66,24 @@ export function packBinaryJson(json, gltfBuilder, objectKey = null, options = {}
 }
 
 function flattenObject(key, object) {
+  let typedArray = null;
+  let size = 3;
+
   if (key === 'vertices' || key === 'points') {
     // Flatten nested vertices
-    const typedArray = flattenToTypedArray(object, 3, Float32Array);
-    if (typedArray) {
-      return {
-        typedArray,
-        size: 3
-      };
-    }
+    typedArray = flattenToTypedArray(object, size, Float32Array);
   }
   if (key === 'colors') {
-    const size = object[0].length === 4 ? 4 : 3;
-    const typedArray = flattenToTypedArray(object, size, Uint8Array);
-    if (typedArray) {
-      return {
-        typedArray,
-        size
-      };
-    }
+    size = object[0].length === 4 ? 4 : 3;
+    typedArray = flattenToTypedArray(object, size, Uint8Array);
   }
+
+  if (typedArray) {
+    return {
+      typedArray,
+      size
+    };
+  }
+
   return null;
 }
