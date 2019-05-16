a6d1cfb815c8877107c3b3133adb33995cde6ef8
Tim Wojtaszek <twojtasz@uber.com>
2019-05-16 02:40:57 -0700

    Rename dropping the "data"

--- modules/ros/src/index.js
+++ modules/ros/src/index.js
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 export {Bag} from './bag';
-export {ROSBAGDataProvider} from './providers/rosbag-data-provider';
+export {ROSBAGProvider} from './providers/rosbag-provider';
