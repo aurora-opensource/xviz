e91320b05fb97b06c800b8a0fc29cfcd43beedbf
Tim Wojtaszek <twojtasz@uber.com>
2019-11-07 14:08:02 -0800

    Add file for ocular size reporting

--- ocular-dev-tools.config.js
+++ ocular-dev-tools.config.js
@@ -38,6 +38,7 @@ module.exports = {
     test: 'test/index.js',
     'test-browser': 'test/browser.js',
     bench: 'test/bench/index.js',
-    'bench-browser': 'test/bench/browser.js'
+    'bench-browser': 'test/bench/browser.js',
+    size: 'test/size/import-nothing.js'
   }
 };
---
+++ test/size/import-nothing.js
@@ -0,0 +1,9 @@
+import {XVIZBuilder} from '@xviz/builder';
+import {XVIZ_MESSAGE_TYPE} from '@xviz/parser';
+import {XVIZData} from '@xviz/io';
+import {XVIZValidator} from '@xviz/schema';
+
+console.log(XVIZBuilder); // eslint-disable-line
+console.log(XVIZ_MESSAGE_TYPE); // eslint-disable-line
+console.log(XVIZData); // eslint-disable-line
+console.log(XVIZValidator); // eslint-disable-line
