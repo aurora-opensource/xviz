15974ecadf1b9503ec0271a510d4c3c2737a5290
Rahul <rahul.ramakrishnan@ravenind.com>
2020-08-14 13:24:32 -0500

    Fixed the argument change

--- python/examples/scenarios/collector_output.py
+++ python/examples/scenarios/collector_output.py
@@ -527,7 +527,7 @@ class CollectorScenario:
             print('Crashed in draw heading:', e)
     
 
-    def get_object_xyz(self, ob, angle_key, dist_key, radar_ob=False):
+    def get_object_xyz(self, ob, angle_key, dist_key, not_radar_ob=False):
         x = math.cos(ob[angle_key]) * ob[dist_key]
         y = math.sin(ob[angle_key]) * ob[dist_key]
         z = 0.5
