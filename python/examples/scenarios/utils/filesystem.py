b52ffde2dcdc4e3fd7122193bf418c8191b15385
Rory Hubbard <calvin.hubbard@ravenind.com>
2020-09-10 11:27:31 -0500

    implemented function to get list of sorted collector_instances

--- python/examples/scenarios/utils/filesystem.py
+++ python/examples/scenarios/utils/filesystem.py
@@ -1,3 +1,19 @@
+import shutil
+from pathlib import Path
+
+
+def get_collector_instances(output_file, extract_directory):
+    output_file = Path(output_file)
+    print("Using collector_output_file:", output_file)
+    extract_directory = Path(extract_directory)
+    print("Using:extract_directory:", extract_directory)
+    if not output_file.is_file():
+        print('collector output file does not exit')
+
+    establish_fresh_directory(extract_directory)
+    shutil.unpack_archive(str(output_file), str(extract_directory))
+
+    return sorted(extract_directory.glob('*.txt'))
 
 
 def establish_fresh_directory(path):
