4b06cfde5240797718acd8d16a8cd6cead0fc613
Rory Hubbard <calvin.hubbard@ravenind.com>
2020-09-30 16:29:38 -0500

    changed print statement

--- python/examples/scenarios/utils/filesystem.py
+++ python/examples/scenarios/utils/filesystem.py
@@ -5,9 +5,9 @@ from pathlib import Path
 
 def get_collector_instances(output_file, extract_directory):
     output_file = Path(output_file)
-    print("Using collector_output_file:", output_file)
+    print("Using collector output file:", output_file)
     extract_directory = Path(extract_directory)
-    print("Using:extract_directory:", extract_directory)
+    print("Using extract directory:", extract_directory)
     if not output_file.is_file():
         print('collector output file does not exit')
 
