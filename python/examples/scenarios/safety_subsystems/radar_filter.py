4e0ae68f4ced31c1b822a0e97a4501b2eeb51b32
Rory Hubbard <calvin.hubbard@ravenind.com>
2020-10-09 15:18:18 -0500

    queue filter is correct now

--- python/examples/scenarios/safety_subsystems/radar_filter.py
+++ python/examples/scenarios/safety_subsystems/radar_filter.py
@@ -13,15 +13,15 @@ def get_radar_filter(config):
     pexist_min = config['confidence_threshold']
     dbpower_min = config['d_bpower_threshold']
     step_max = config['step_max']
-    queue_size = 2
+    queue_length = 2
 
-    return RadarFilter(pfilter_enabled, qfilter_enabled, queue_size, consecutive_min,
+    return RadarFilter(pfilter_enabled, qfilter_enabled, queue_length, consecutive_min,
                                             pexist_min, dbpower_min, phi_sdv_max, step_max)
 
 
 class RadarFilter:
 
-    def __init__(self, pfilter_enabled, qfilter_enabled, queue_size, consecutive_min,
+    def __init__(self, pfilter_enabled, qfilter_enabled, queue_length, consecutive_min,
                                         pexist_min, dbpower_min, phi_sdv_max, step_max):
 
         if not (pfilter_enabled or qfilter_enabled):
@@ -29,7 +29,7 @@ class RadarFilter:
 
         self.pfilter_enabled = pfilter_enabled
         self.qfilter_enabled = qfilter_enabled
-        self.queue_size = queue_size
+        self.queue_length = queue_length
         self.consecutive_min = consecutive_min
         self.pexist_min = pexist_min
         self.dbpower_min = dbpower_min
@@ -41,7 +41,7 @@ class RadarFilter:
     
     def update_queue(self, target_id, target):
         if target_id not in self.queues:
-            self.queues[target_id] = QState(self.queue_size)
+            self.queues[target_id] = QState(self.queue_length)
         q_state = self.queues[target_id]
         q_state.update_state(target)
 
@@ -52,9 +52,11 @@ class RadarFilter:
             is_valid = self.passive_filter(target)
         
         if self.qfilter_enabled:
+            self.target_id_set.remove(target['targetId'])
             self.update_queue(target['targetId'], target)
 
-            if is_valid: # only apply the queue filter if the target passed through the passive filter
+            # only apply the queue filter if the target passed through the passive filter
+            if is_valid:
                 is_valid = self.queue_filter(target)
 
         return is_valid
@@ -75,11 +77,14 @@ class RadarFilter:
             Returns True if the target is valid.
         '''
         q_state = self.queues[target['targetId']]
-        return not any(
-            step is None
-            or step > self.step_max
-            for step in q_state.steps
-        )
+        queue_count = 0
+        for step in q_state.steps:
+            if step is None or step > self.step_max:
+                return False
+            queue_count += 1
+        if queue_count < self.queue_length:
+            return False
+        return True
 
     def filter_targets_until_path_prediction(self, target, in_sync=True, is_combine=False, at_sync_point=False):
         if in_sync and not at_sync_point:
@@ -97,11 +102,11 @@ class RadarFilter:
 
 class QState:
 
-    def __init__(self, queue_size):
+    def __init__(self, queue_length):
         self.prev_dr = None
         self.prev_phi = None
         self.prev_consecutive = None
-        self.steps = deque(maxlen=queue_size)
+        self.steps = deque(maxlen=queue_length)
 
     def is_duplicate_target(self, target):
         if self.prev_consecutive is None \
@@ -131,7 +136,7 @@ class QState:
     def update_state(self, target):
         if self.is_duplicate_target(target):
             return
-        if target['consecutive']:
+        if target['consecutive'] < 1:
             self.update_with_default_target()
         else:
             self.update_with_measured_target(target)
