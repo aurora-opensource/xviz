8be3f2bd782c22b4e5c8bb059b9ac8774bc0e985
Rory Hubbard <calvin.hubbard@ravenind.com>
2021-03-18 18:39:22 -0500

    checkout for guidance_sim script

--- python/examples/guidance_sim.py
+++ python/examples/guidance_sim.py
@@ -1,14 +1,56 @@
 import sys
 import math
 from pathlib import Path
+import matplotlib.pyplot as plt
 from scenarios.utils.filesystem import get_collector_instances, load_config
 from scenarios.utils.read_protobufs import deserialize_collector_output, \
-    extract_collector_output, extract_collector_output_slim
+    extract_collector_output_slim
 from scenarios.utils.gis import lonlat_to_utm, get_wheel_angle
 
-sys.path.append('../../SmartHP-v2')
+sys.path.append(str(Path(__file__).parents[2] / 'SmartHP-v2'))
 from smarthp.gnc.guidance import Guidance, CTEGuidanceTask
 
+
+plt.rcParams['figure.figsize'] = [15, 10]
+plt.rcParams['savefig.facecolor'] = 'black'
+plt.rcParams['figure.facecolor'] = 'black'
+plt.rcParams['figure.edgecolor'] = 'white'
+plt.rcParams['axes.facecolor'] = 'black'
+plt.rcParams['axes.edgecolor'] = 'white'
+plt.rcParams['axes.labelcolor'] = 'white'
+plt.rcParams['axes.titlecolor'] = 'white'
+plt.rcParams['xtick.color'] = 'white'
+plt.rcParams['ytick.color'] = 'white'
+# plt.rcParams['legend.facecolor'] = 'white'
+plt.rcParams['text.color'] = 'white'
+plt.rcParams["figure.autolayout"] = True
+
+
+def get_planned_path(collector_instances):
+    for collector_instance in collector_instances:
+
+        collector_output, is_slim_output = deserialize_collector_output(
+            collector_instance)
+
+        if is_slim_output:
+            _, _, _, _, _, _, planned_path, _, _, _ \
+                = extract_collector_output_slim(collector_output)
+        else:
+            print('collector file is not campatible for running guidance sim')
+
+        if planned_path is not None:
+            if planned_path.size > 0:
+                return planned_path.reshape(-1, 2)
+
+    print('planned path never found in collector file')
+    return None
+
+
+def get_value_list(dict_list, k):
+    '''
+    return list of values for given key from a list of dicts
+    '''
+    return list(map(lambda x: x[k], dict_list))
 
 
 def main():
@@ -21,10 +63,20 @@ def main():
     collector_instances = get_collector_instances(collector_output_file,
                                                   extract_directory)
 
-    utm_zone = None
-    tractor_state = None
-    combine_states = dict()
-    planned_path = None
+    if collector_config['IVT']:
+        configfile = Path(__file__).parents[3] / 'Global-Configs' / \
+            'Tractors' / 'John-Deere' / '8RIVT_WHEEL.yaml'
+    else:
+        configfile = Path(__file__).parents[3] / 'Global-Configs' / \
+            'Tractors' / 'John-Deere' / '8RPST_WHEEL.yaml'
+
+    global_config = load_config(str(configfile))
+
+    planned_path = get_planned_path(collector_instances)
+    waypoints = list(map(tuple, planned_path))
+
+    guidance_states = []
+    control_signals = []
 
     for collector_instance in collector_instances:
 
@@ -32,70 +84,73 @@ def main():
             collector_instance)
 
         if is_slim_output:
-            img, camera_output, radar_output, tracking_output, \
-                machine_state, field_definition, planned_path, \
-                sync_status, control_signal, sync_params \
+            _, _, _, _, machine_state, _, _, _, control_signal, _ \
                 = extract_collector_output_slim(collector_output)
         else:
-            img, camera_output, radar_output, tracking_output, \
-                machine_state = extract_collector_output(collector_output)
-            field_definition = None
-            planned_path = None
-            sync_status = None
-            control_signal = None
-            sync_params = None
+            print('collector file is not campatible for running guidance sim')
+            return
 
         if machine_state is not None:
             utm_zone = machine_state['opState']['refUtmZone']
             vehicle_states = machine_state['vehicleStates']
+            tractor_state_updated = False
             for vehicle, state in vehicle_states.items():
                 if vehicle == 'tractor':
                     tractor_state = state
-                else:
-                    combine_states[vehicle] = state
+                    tractor_state_updated = True
+                    break
+            if not tractor_state_updated:
+                continue
+        else:
+            continue
         
-        if tractor_state:
-            tractor_theta = (90 - tractor_state['heading']) * math.pi / 180
-            tractor_easting, tractor_northing = lonlat_to_utm(
-                tractor_state['longitude'],
-                tractor_state['latitude'],
-                utm_zone,
-            )
-            guidance_state = dict(
-                utm_pos=(tractor_easting, tractor_northing),
-                heading=tractor_state['heading'],
-                veh_speed=tractor_state['speed'],
-            )
-
-        if planned_path is not None:
-            if planned_path.size > 0:
-                planned_path = planned_path.reshape(-1, 2)
-            else:
-                planned_path = None
-
-        if field_definition is not None:
-            field_definition = field_definition
+        tractor_theta = (90 - tractor_state['heading']) * math.pi / 180
+        tractor_easting, tractor_northing = lonlat_to_utm(
+            tractor_state['longitude'],
+            tractor_state['latitude'],
+            utm_zone,
+        )
+        guidance_state = dict(
+            utm_pos=(tractor_easting, tractor_northing),
+            heading=tractor_state['heading'],
+            veh_speed=tractor_state['speed'],
+        )
+        guidance_states.append(guidance_state)
 
         if control_signal is not None:
-            control_signal = control_signal
+            control_signals.append(control_signal)
 
-        if sync_status is not None:
-            sync_status = sync_status
-        
-        if sync_params is not None:
-            if sync_params:
-                sync_params = sync_params
-            else:
-                sync_params = None
+    simulated_control_signals = simulate_guidance(guidance_states,
+                                                  waypoints, global_config)
 
-    # path = _____
-    # config = _____
-    # guidance = Guidance()
-    # route_task = guidance.CTEGuidanceTask(
-    #     path,
-    #     config=config,
-    #     stop_on_target=True)
-    # guidance.add_task(route_task)
+    return
+    fig, ax = plt.subplots(nrows=1, ncols=2)
+
+    tractor_pos = get_value_list(guidance_states, 'utm_pos')
+    ax[0].plot(*zip(*waypoints))
+    ax[0].plot(*zip(*tractor_pos))
+
+    commanded_speeds = get_value_list(control_signals, 'setSpeed')
+    commanded_curvatures = get_value_list(control_signals, 'commandCurvature')
+    ax[1].plot(commanded_speeds)
+
+    plt.show()
+    plt.close()
+
+
+def simulate_guidance(guidance_states, waypoints, global_config):
+    guidance = Guidance()
+    route_task = CTEGuidanceTask(
+        waypoints,
+        config=global_config,
+        stop_on_target=True)
+    guidance.add_task(route_task)
+    guidance.start_pending()
+    guidance.set_config(global_config)
+
+    for gs in guidance_states:
+        simulated_control = guidance.run(gs)
+        print(simulated_control)
 
 
 if __name__ == '__main__':
