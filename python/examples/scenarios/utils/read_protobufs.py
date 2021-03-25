84e65aecafa30f64438aed18d748efad26ad3b79
Rory Hubbard <calvin.hubbard@ravenind.com>
2021-03-25 11:29:44 -0500

    correlating camera outputs to images and return single dict camera_data out of extract_collector_output_slimg

--- python/examples/scenarios/utils/read_protobufs.py
+++ python/examples/scenarios/utils/read_protobufs.py
@@ -8,10 +8,7 @@ from protobuf_APIs import collector_pb2, gandalf_pb2, falconeye_pb2, \
     radar_pb2, camera_pb2, smarthp_pb2
 
 from .image import extract_image
-from .com_manager import MqttConst
-
-
-NUM_OF_CAMS = 6
+from .com_manager import MqttConst as mc
 
 
 def deserialize_collector_output(file_path):
@@ -30,39 +27,19 @@ def deserialize_collector_output(file_path):
 
 def extract_collector_output_slim(collector_output):
     try:
-        frames = []
-        if 'frame_cam_0' in collector_output.data:
-            for i in range(NUM_OF_CAMS):
-                key = 'frame_cam_' + str(i)
-                if key in collector_output.data:
-                    frame = extract_image(collector_output.data[key])
-                    frames.append((i, frame))
-        elif 'frame' in collector_output.data:
-            frame = extract_image(collector_output.data['frame'])
-            frames.append((0, frame))
-        else:
-            print('missing frame from collector output')
-            frame = None
-
-        if MqttConst.CAMERA_TOPIC in collector_output.data:
-            camera_output = camera_pb2.CameraOutput()
-            camera_output.ParseFromString(collector_output.data[MqttConst.CAMERA_TOPIC])
-            camera_output = MessageToDict(camera_output, including_default_value_fields=True)
-        else:
-            print('missing camera output from collector output')
-            camera_output = None
-
-        if MqttConst.RADAR_TOPIC in collector_output.data:
+        if mc.RADAR_TOPIC in collector_output.data:
             radar_output = radar_pb2.RadarOutput()
-            radar_output.ParseFromString(collector_output.data[MqttConst.RADAR_TOPIC])
+            radar_output.ParseFromString(collector_output.data[mc.RADAR_TOPIC])
             radar_output = MessageToDict(radar_output, including_default_value_fields=True)
+            del collector_output.data[mc.RADAR_TOPIC]
         else:
             radar_output = None
 
-        if MqttConst.TRACKS_TOPIC in collector_output.data:
+        if mc.TRACKS_TOPIC in collector_output.data:
             tracking_output = falconeye_pb2.TrackingOutput()
-            tracking_output.ParseFromString(collector_output.data[MqttConst.TRACKS_TOPIC])
+            tracking_output.ParseFromString(collector_output.data[mc.TRACKS_TOPIC])
             tracking_output = MessageToDict(tracking_output, including_default_value_fields=True)
+            del collector_output.data[mc.TRACKS_TOPIC]
         else:
             tracking_output = None
 
@@ -70,22 +47,26 @@ def extract_collector_output_slim(collector_output):
             machine_state = gandalf_pb2.MachineState()
             machine_state.ParseFromString(collector_output.data['collector/data/machine_state'])
             machine_state = MessageToDict(machine_state, including_default_value_fields=True)
+            del collector_output.data['collector/data/machine_state']
         else:
             machine_state = None
 
         if 'collector/data/field_def' in collector_output.data:
             field_definition = collector_output.data['collector/data/field_def']
             field_definition = json.loads(field_definition.decode('ascii'))
-        else:
+            del collector_output.data['collector/data/field_def']
+        else:  # maintain backwards compatibility by checking for old key
             if 'collector/data/field_definition' in collector_output.data:
                 field_definition = collector_output.data['collector/data/field_definition']
                 field_definition = json.loads(field_definition.decode('ascii'))
+                del collector_output.data['collector/data/field_definition']
             else:
                 field_definition = None
 
         if 'collector/data/planned_path' in collector_output.data:
             planned_path = collector_output.data['collector/data/planned_path']
             planned_path = np.frombuffer(planned_path, dtype=np.float_)
+            del collector_output.data['collector/data/planned_path']
         else:
             planned_path = None
 
@@ -93,11 +74,13 @@ def extract_collector_output_slim(collector_output):
             sync_status = smarthp_pb2.SyncStatus()
             sync_status.ParseFromString(collector_output.data['collector/data/sync'])
             sync_status = MessageToDict(sync_status, including_default_value_fields=True)
-        else:
+            del collector_output.data['collector/data/sync']
+        else: # maintain backwards compatibility by checking for old key
             if 'collector/data/sync_status' in collector_output.data:
                 sync_status = smarthp_pb2.SyncStatus()
                 sync_status.ParseFromString(collector_output.data['collector/data/sync_status'])
                 sync_status = MessageToDict(sync_status, including_default_value_fields=True)
+                del collector_output.data['collector/data/sync_status']
             else:
                 sync_status = None
 
@@ -105,19 +88,68 @@ def extract_collector_output_slim(collector_output):
             control_signal = gandalf_pb2.ControlSignal()
             control_signal.ParseFromString(collector_output.data['collector/data/control_signal'])
             control_signal = MessageToDict(control_signal, including_default_value_fields=True)
+            del collector_output.data['collector/data/control_signal']
         else:
             control_signal = None
 
         if 'collector/data/sync_params' in collector_output.data:
             sync_params = collector_output.data['collector/data/sync_params']
             sync_params = json.loads(sync_params.decode('ascii'))
+            del collector_output.data['collector/data/sync_params']
         else:
             sync_params = None
+
+        # {<key associated to frame>: (frame, corresponding CameraOutput)}
+        camera_data = dict()
+        # Collector appends the camera index to the key for the image
+        # Primary camera is assumed to have index 0
+        if 'frame_cam_0' in collector_output.data:
+            for key in collector_output.data:
+                if 'frame_cam_' in key:
+                    frame = extract_image(collector_output.data[key])
+                    cam_idx = key.split('_')[-1]
+                    if int(cam_idx) == 0:  # primary camera
+                        if mc.CAMERA_TOPIC in collector_output.data:
+                            camera_output = camera_pb2.CameraOutput()
+                            camera_output.ParseFromString(
+                                collector_output.data[mc.CAMERA_TOPIC])
+                            camera_output = MessageToDict(
+                                camera_output,
+                                including_default_value_fields=True)
+                        else:
+                            print('missing primary camera output from collector output')
+                            camera_output = None
+                    else:  # hazard camera
+                        haz_cam_output_key = mc.HAZARD_CAMERA_TOPIC + '_' + cam_idx
+                        if haz_cam_output_key  in collector_output.data:
+                            camera_output = camera_pb2.CameraOutput()
+                            camera_output.ParseFromString(
+                                collector_output.data[haz_cam_output_key])
+                            camera_output = MessageToDict(
+                                camera_output,
+                                including_default_value_fields=True)
+                        else:
+                            print('missing primary camera output from collector output')
+                            camera_output = None
+                    camera_data[key] = (frame, camera_output)
+        elif 'frame' in collector_output.data:
+            frame = extract_image(collector_output.data['frame'])
+            if mc.CAMERA_TOPIC in collector_output.data:
+                camera_output = camera_pb2.CameraOutput()
+                camera_output.ParseFromString(collector_output.data[mc.CAMERA_TOPIC])
+                camera_output = MessageToDict(camera_output, including_default_value_fields=True)
+            else:
+                print('missing primary camera output from collector output')
+                camera_output = None
+            camera_data['frame_cam_0'] = (frame, camera_output)
+        else:
+            print('missing primary camera frame from collector output')
+            frame = None
 
     except Exception as e:
         print('failed to extract collector output:', e)
 
-    return frames, camera_output, radar_output, tracking_output, machine_state, \
+    return camera_data, radar_output, tracking_output, machine_state, \
             field_definition, planned_path, sync_status, control_signal, sync_params
 
 
